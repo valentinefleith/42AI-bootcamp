eda1911a08db426c1cdbe0123ed3ab0b1b6097fa
valentinefleith <valentine.fleith@gmail.com>
2024-03-13 12:41:16 +0100

    add structure

---
+++ Module02/ex02/logger.py
@@ -0,0 +1,54 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    logger.py                                          :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: vafleith <vafleith@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/03/13 12:35:43 by vafleith          #+#    #+#              #
+#    Updated: 2024/03/13 12:41:09 by vafleith         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+import time
+from random import randint
+import os
+
+#... your definition of log decorator...
+class CoffeeMachine():
+
+    water_level = 100
+
+    @log
+    def start_machine(self):
+        if self.water_level > 20:
+            return True
+        else:
+            print("Please add water!")
+            return False
+
+    @log
+    def boil_water(self):
+        return "boiling..."
+
+    @log
+    def make_coffee(self):
+        if self.start_machine():
+            for _ in range(20):
+                time.sleep(0.1)
+                self.water_level -= 1
+            print(self.boil_water())
+            print("Coffee is ready!")
+
+    @log
+    def add_water(self, water_level):
+        time.sleep(randint(1, 5))
+        self.water_level += water_level
+        print("Blub blub blub...")
+
+if __name__ == "__main__":
+    machine = CoffeeMachine()
+    for i in range(0, 5):
+        machine.make_coffee()
+    machine.make_coffee()
+    machine.add_water(70)
