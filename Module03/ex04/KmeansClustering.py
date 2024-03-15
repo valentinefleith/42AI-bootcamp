046b9eb273993d588602c2684ef43c7bd9d8f3b6
valentinefleith <valentine.fleith@gmail.com>
2024-03-15 23:50:16 +0100

    add structure for ex4

---
+++ Module03/ex04/KmeansClustering.py
@@ -0,0 +1,49 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    KmeansClustering.py                                :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: vafleith <vafleith@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/03/15 23:46:43 by vafleith          #+#    #+#              #
+#    Updated: 2024/03/15 23:48:18 by vafleith         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+class KmeansClustering:
+    
+    def __init__(self, max_iter=20, ncentroid=5):
+        self.ncentroid = ncentroid # number of centroids
+        self.max_iter = max_iter # number of max iterations to update the centroids
+        self.centroids = [] # values of the centroids
+
+    def fit(self, X):
+        """
+        Run the K-means clustering algorithm.
+        For the location of the initial centroids, random pick ncentroids from the dataset.
+        Args:
+        -----
+        X: has to be an numpy.ndarray, a matrice of dimension m * n.
+        Return:
+        -------
+        None.
+        Raises:
+        -------
+        This function should not raise any Exception.
+        """
+        #... your code ...
+
+    def predict(self, X):
+        """
+        Predict from wich cluster each datapoint belongs to.
+        Args:
+        -----
+        X: has to be an numpy.ndarray, a matrice of dimension m * n.
+        Return:
+        -------
+        the prediction has a numpy.ndarray, a vector of dimension m * 1.
+        Raises:
+        -------
+        This function should not raise any Exception.
+        """
+        #... your code ...
