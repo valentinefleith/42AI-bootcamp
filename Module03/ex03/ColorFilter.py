c31a985bcf8d53b14cdeea68d2918bea1dee0d28
valentinefleith <valentine.fleith@gmail.com>
2024-03-15 23:40:44 +0100

    add grayscale

--- Module03/ex03/ColorFilter.py
+++ Module03/ex03/ColorFilter.py
@@ -6,7 +6,7 @@
 #    By: vafleith <vafleith@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/03/15 17:24:38 by vafleith          #+#    #+#              #
-#    Updated: 2024/03/15 20:19:45 by vafleith         ###   ########.fr        #
+#    Updated: 2024/03/15 23:40:15 by vafleith         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -112,10 +112,53 @@ class ColorFilter:
         celluloid = np.copy(array)
         lower_threshold = thresholds[0]
         for upper_threshold in thresholds[:1]:
-            mask = (celluloid[..., :3] > lower_threshold) & (celluloid[..., :3] < upper_threshold)
+            mask = (celluloid[..., :3] > lower_threshold) & (
+                celluloid[..., :3] < upper_threshold
+            )
             celluloid[..., :3][mask] = lower_threshold
             lower_threshold = upper_threshold
         return celluloid
+
+    def to_grayscale(self, array, filter, **kwargs):
+        """
+        Applies a grayscale filter to the image received as a numpy array.
+        For filter = ’mean’/’m’: performs the mean of RBG channels.
+        For filter = ’weight’/’w’: performs a weighted mean of RBG channels.
+        Args:
+        -----
+            array: numpy.ndarray corresponding to the image.
+            filter: string with accepted values in [’m’,’mean’,’w’,’weight’]
+            weights: [kwargs] list of 3 floats where the sum equals to 1,
+            corresponding to the weights of each RBG channels.
+        Return:
+        -------
+            array: numpy.ndarray corresponding to the transformed image.
+            None: otherwise.
+        Raises:
+        -------
+            This function should not raise any Exception.
+        """
+        if filter not in ["m", "mean", "w", "weight"]:
+            return None
+        grayscale_filter = np.copy(array)
+        if filter in ["m", "mean"]:
+            grayscale_filter[..., 0:3] = np.sum(
+                grayscale_filter[..., 0:3] / 3, axis=2, keepdims=True
+            ).astype(grayscale_filter.dtype)
+            return grayscale_filter
+        weights = kwargs.get("weights")
+        if len(weights) != 3:
+            return None
+        grayscale_filter[:, :, 0:3] = np.sum(
+            [
+                grayscale_filter[:, :, 0:1] * weights[0],
+                weights[1] * array[:, :, 1:2],
+                weights[2] * grayscale_filter[:, :, 2:3],
+            ],
+            axis=0,
+        )
+        return grayscale_filter
+
 
 def main():
     imp = ImageProcessor()
@@ -135,9 +178,11 @@ def main():
     # red_filter = cf.to_red(arr)
     # imp.display(red_filter)
     ##### CELLULOID TEST #####
-    celluloid_filter = cf.to_celluloid(arr)
-    imp.display(celluloid_filter)
-    
+    # celluloid_filter = cf.to_celluloid(arr)
+    # imp.display(celluloid_filter)
+    ##### GRAYSCALE TEST #####
+    grayscale_filter = cf.to_grayscale(arr, "w", weights=[0.005, 0.05, 0.9])
+    imp.display(grayscale_filter)
 
 
 if __name__ == "__main__":
