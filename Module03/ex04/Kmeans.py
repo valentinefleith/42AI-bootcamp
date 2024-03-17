573ce4273dae0b911cf6683b3c045e0b3ec7d39c
valentinefleith <valentine.fleith@gmail.com>
2024-03-17 03:00:32 +0100

    add kmeans algorithm

--- Module03/ex04/Kmeans.py
+++ Module03/ex04/Kmeans.py
@@ -6,7 +6,7 @@
 #    By: vafleith <vafleith@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/03/15 23:46:43 by vafleith          #+#    #+#              #
-#    Updated: 2024/03/16 20:32:17 by vafleith         ###   ########.fr        #
+#    Updated: 2024/03/17 02:29:43 by vafleith         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,12 +16,19 @@ import sys
 import random
 import matplotlib.pyplot as plt
 
+
 class KmeansClustering:
     
     def __init__(self, max_iter=20, ncentroid=5):
-        self.ncentroid = ncentroid # number of centroids
-        self.max_iter = max_iter # number of max iterations to update the centroids
-        self.centroids = [] # values of the centroids
+        self.ncentroid = ncentroid  # number of centroids
+        self.max_iter = max_iter  # number of max iterations to update the centroids
+        self.centroids = []  # values of the centroids
+
+    def init_centroids(self, X):
+        min_val, max_val = np.min(X, axis=0), np.max(X, axis=0)
+        self.centroids = [random.uniform(min_val, max_val) for _ in range(self.ncentroid)]
+        # for _ in range(self.ncentroid):
+        #     self.centroids.append(random.choice(X))
 
     def fit(self, X):
         """
@@ -37,8 +44,22 @@ class KmeansClustering:
         -------
         This function should not raise any Exception.
         """
-        for _ in range(self.ncentroid):
-            self.centroids.append(random.choice(X))
+        self.init_centroids(X)
+        prev_centroids = None
+        for _ in range(self.max_iter):
+            sorted_points = self.predict(X)
+            prev_centroids = self.centroids
+            self.centroids = [np.mean(cluster, axis=0) for cluster in sorted_points]
+            # print("Centroids shape:", [centroid.shape for centroid in self.centroids])
+            # print("Prev centroids shape:", [centroid.shape for centroid in prev_centroids])
+            for i, centroid in enumerate(self.centroids):
+                if np.isnan(centroid).any():
+                    self.centroids[i] = prev_centroids[i]
+            # print("Centroids shape:", [centroid.shape for centroid in self.centroids])
+            if np.equal(self.centroids, prev_centroids).all():
+                break
+        # print(sorted_points)
+
 
     def predict(self, X):
         """
@@ -53,14 +74,21 @@ class KmeansClustering:
         -------
         This function should not raise any Exception.
         """
-        #... your code ...
+        sorted_points = [[] for _ in range(self.ncentroid)]
+        for value in X:
+            distances = []
+            for centroid in self.centroids:
+                distances.append(self.cosine_similarity(value, centroid))
+            centroid_index = np.argmin(distances)
+            sorted_points[centroid_index].append(value)
+        return sorted_points
 
     @staticmethod
     def cosine_similarity(a, b):
         """
         Input :
-        a : a numpy array representing word a as a vector
-        b : a numpy array representing word b as a vector
+        a : a numpy array representing data a as a vector
+        b : a numpy array representing data b as a vector
         Output:
         cos_ab : a scalar proportional to the the similarity in angles between a and b
         """
@@ -78,22 +106,43 @@ def parse_args(args):
             max_iter = arg.split('=')[1]
     if filepath is None:
         sys.exit("ERROR : Filepath is mandatory.")
-    if ncentroid is None:
+    if ncentroid is None or int(ncentroid) <= 1:
         ncentroid = 5
-    if max_iter is None:
+    if max_iter is None or int(max_iter) <= 1:
         max_iter = 20
     return filepath, ncentroid, max_iter
 
 
 def plot_data(data):
-    x = data[:, 1]
-    y = data[:, 2]
-    z = data[:, 3]
+    x = data[:, 0]
+    y = data[:, 1]
+    z = data[:, 2]
     fig = plt.figure(figsize=(12, 7))
     ax = fig.add_subplot(projection='3d')
     ax.scatter(x, y, z)
     plt.show()
 
+
+def plot_sorted_points(data, prediction):
+    x = data[:, 0]
+    y = data[:, 1]
+    z = data[:, 2]
+    fig = plt.figure(figsize=(12, 7))
+    ax = fig.add_subplot(projection='3d')
+    for valx, valy, valz in zip(x, y, z):
+        for i, cluster in enumerate(prediction):
+            if np.equal(np.array([valx, valy, valz]), cluster).any():
+            #if np.array([valx, valy, valz]).all() in cluster.any():
+                if i == 0:
+                    ax.scatter(valx, valy, valz, color='black')
+                if i == 1:
+                    ax.scatter(valx, valy, valz, color='green')
+                if i == 2:
+                    ax.scatter(valx, valy, valz, color='red')
+                if i == 3:
+                    ax.scatter(valx, valy, valz, color='blue')
+    ax.scatter(x, y, z)
+    plt.show()
 
 def main():
     if not 1 < len(sys.argv) < 5:
@@ -101,9 +150,13 @@ def main():
     filepath, ncentroid, max_iter = parse_args(sys.argv[1:])
     with CsvReader(filepath, skip_top=1) as file:
         data = np.array(file.getdata(), dtype=float)
-    plot_data(data)
+    data = data[:, 1:]
+    # plot_data(data)
     kmc = KmeansClustering(ncentroid=ncentroid, max_iter=max_iter)
     kmc.fit(data)
+    prediction = kmc.predict(data)
+    #print(prediction)
+    plot_sorted_points(data, prediction)
 
 if __name__ == "__main__":
     main()
