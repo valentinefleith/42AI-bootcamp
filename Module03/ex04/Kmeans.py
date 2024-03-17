3038b3bc1a79b95af80da1d58a3fe47e20682ce8
valentinefleith <valentine.fleith@gmail.com>
2024-03-17 17:47:32 +0100

    update format

--- Module03/ex04/Kmeans.py
+++ Module03/ex04/Kmeans.py
@@ -18,7 +18,6 @@ import matplotlib.pyplot as plt
 
 
 class KmeansClustering:
-
     def __init__(self, max_iter=20, ncentroid=5):
         self.ncentroid = ncentroid
         self.max_iter = max_iter
@@ -52,7 +51,7 @@ class KmeansClustering:
                 if np.isnan(centroid).any():
                     self.centroids[i] = prev_centroids[i]
             if not (np.array(prev_centroids) - np.array(self.centroids)).any():
-            #if np.equal(self.centroids, prev_centroids).all():
+                # if np.equal(self.centroids, prev_centroids).all():
                 break
 
     def closest_centroid(self, x):
@@ -118,11 +117,11 @@ def parse_args(args):
     filepath, ncentroid, max_iter = None, None, None
     for arg in args:
         if arg.startswith("filepath=") and len(arg) > len("filepath="):
-            filepath = arg.split('=')[1]
+            filepath = arg.split("=")[1]
         if arg.startswith("ncentroid=") and len(arg) > len("ncentroid="):
-            ncentroid = int(arg.split('=')[1])
+            ncentroid = int(arg.split("=")[1])
         if arg.startswith("max_iter=") and len(arg) > len("max_iter="):
-            max_iter = int(arg.split('=')[1])
+            max_iter = int(arg.split("=")[1])
     if filepath is None:
         sys.exit("ERROR : Filepath is mandatory.")
     if ncentroid is None or int(ncentroid) <= 1:
@@ -137,15 +136,15 @@ def plot_data(data):
     y = data[:, 1]
     z = data[:, 2]
     fig = plt.figure(figsize=(12, 7))
-    ax = fig.add_subplot(projection='3d')
+    ax = fig.add_subplot(projection="3d")
     ax.scatter(x, y, z)
     plt.show()
 
 
 def plot_sorted_points(prediction):
     fig = plt.figure(figsize=(12, 8))
-    ax = fig.add_subplot(projection='3d')
-    colors = ['yellow', 'green', 'red', 'darkorchid', 'blue', 'orange', 'black']
+    ax = fig.add_subplot(projection="3d")
+    colors = ["yellow", "green", "red", "darkorchid", "blue", "orange", "black"]
     for i, cluster in enumerate(prediction):
         for data in cluster:
             x, y, z = data
